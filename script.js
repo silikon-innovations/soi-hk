231af5276b6956dab5686d5846cd5e4c6d356a47
vedvardhan gyanmote <vedvardhan.g@gmail.com>
2020-06-13 20:35:05 +0530

    Changed registration dates

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jun 14, 2020 00:00:00").getTime();
+var countDownDate = new Date("Jun 15, 2020 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
